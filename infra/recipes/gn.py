e375226345b2be2bfc8c4549702ee3e437b5c136
Petr Hosek <phosek@google.com>
2018-06-08 16:10:17 -0700

    Update recipe to actually fetch and build changes
    Currently, we only test the bootstrap.
    
    Change-Id: I63c0aaf3f5c7f8b0abe52d480517515844b332ef
    Reviewed-on: https://gn-review.googlesource.com/1260
    Reviewed-by: Scott Graham <scottmg@chromium.org>
    Commit-Queue: Petr Hosek <phosek@google.com>

--- infra/recipes/gn.py
+++ infra/recipes/gn.py
@@ -4,18 +4,73 @@
 """Recipe for building GN."""
 
 DEPS = [
+    'recipe_engine/buildbucket',
+    'recipe_engine/cipd',
+    'recipe_engine/context',
     'recipe_engine/path',
     'recipe_engine/platform',
+    'recipe_engine/properties',
+    'recipe_engine/python',
     'recipe_engine/step',
 ]
 
 
 def RunSteps(api):
   src_dir = api.path['start_dir'].join('gn')
-  api.step('checkout',
-           ['git', 'clone', 'https://gn.googlesource.com/gn', src_dir])
+
+  with api.step.nest('git'), api.context(infra_steps=True):
+    api.step('init', ['git', 'init', src_dir])
+
+    with api.context(cwd=src_dir):
+      build_input = api.buildbucket.build_input
+      if build_input.gerrit_changes:
+        for change in build_input.gerrit_changes:
+          api.step('fetch', [
+              'git', 'fetch',
+              'https://%s/gn' % change.host,
+              'refs/changes/%s/%s/%s' %
+              (str(change.change)[-2:], change.change, change.patchset)
+          ])
+          api.step('cherry-pick', ['git', 'cherry-pick', 'FETCH_HEAD'])
+      else:
+        ref = (
+            build_input.gitiles_commit.id
+            if build_input.gitiles_commit else 'refs/heads/master')
+        api.step('fetch',
+                 ['git', 'fetch', 'https://gn.googlesource.com/gn', ref])
+        api.step('checkout', ['git', 'checkout', 'FETCH_HEAD'])
+
+  with api.context(infra_steps=True):
+    cipd_dir = api.path['start_dir'].join('cipd')
+    packages = {
+      'infra/ninja/${platform}': 'version:1.8.2',
+    }
+    api.cipd.ensure(cipd_dir, packages)
+
+  with api.context(env_prefixes={'PATH': [cipd_dir]}):
+    api.python(
+        'bootstrap',
+        src_dir.join('tools', 'gn', 'bootstrap', 'bootstrap.py'),
+        args=['--no-rebuild'])
 
 
 def GenTests(api):
   for platform in ('linux', 'mac', 'win'):
-    yield api.test(platform) + api.platform.name(platform)
+    yield (api.test('ci_' + platform) + api.platform.name(platform) +
+           api.properties(buildbucket={
+               'build': {
+                   'tags': [
+                       'buildset:commit/gitiles/gn.googlesource.com/gn/+/'
+                       'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+                   ]
+               }
+           }))
+    yield (
+        api.test('cq_' + platform) + api.platform.name(platform) +
+        api.properties(buildbucket={
+            'build': {
+                'tags': [
+                    'buildset:patch/gerrit/gn-review.googlesource.com/1000/1',
+                ]
+            }
+        }))
