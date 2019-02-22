0ac93dd23bafdae516d4d5318c7b748c5c98b489
Khouri Giordano <kirmeo@gmail.com>
2019-02-22 11:01:35 -0500

    For proper order put -lpthreads as a 'library' not a 'linker flag'
    Linker flags appear early among the link command line arguments,
    before other libraries that need pthreads. Changing it to a library
    puts it at the end of the command line.
    
    Change-Id: I2d0cc5e44a33d72f9fcaa67f3a6fbc0da565a735
    Reviewed-on: https://gn-review.googlesource.com/c/4060
    Reviewed-by: Brett Wilson <brettw@chromium.org>
    Commit-Queue: Brett Wilson <brettw@chromium.org>

--- build/gen.py
+++ build/gen.py
@@ -334,7 +334,7 @@ def WriteGNNinja(path, platform, host, options):
       ldflags.append('-maix64')
 
     if platform.is_posix():
-      ldflags.append('-lpthread')
+      libs.append('-lpthread')
 
     if options.use_lto:
       cflags.extend(['-flto', '-fwhole-program-vtables'])
