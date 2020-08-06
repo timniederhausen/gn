03df22eb2727dc732928fd0901c3a0cd46c689d7
Paul Daniel Faria <paulfaria@google.com>
2020-08-06 15:43:43 -0400

    [rust-project] Add "proc_macro" as dep for proc macro targets
    This CL also fixes a bug in the rust_tool selection.
    
    Change-Id: Ib7a87aaa83f72a332158c56f12f70cdfed05cf98
    Reviewed-on: https://gn-review.googlesource.com/c/gn/+/9620
    Commit-Queue: Petr Hosek <phosek@google.com>
    Reviewed-by: Petr Hosek <phosek@google.com>

--- src/gn/rust_project_writer.cc
+++ src/gn/rust_project_writer.cc
@@ -281,7 +281,7 @@ void AddTarget(const BuildSettings* build_settings,
   // Check what sysroot this target needs.  Add it to the crate list if it
   // hasn't already been added.
   auto rust_tool =
-      target->toolchain()->GetToolForSourceTypeAsRust(SourceFile::SOURCE_RS);
+      target->toolchain()->GetToolForTargetFinalOutputAsRust(target);
   auto current_sysroot = rust_tool->GetSysroot();
   if (current_sysroot != "" && sysroot_lookup.count(current_sysroot) == 0) {
     AddSysroot(build_settings, current_sysroot, sysroot_lookup, crate_list);
@@ -332,6 +332,12 @@ void AddTarget(const BuildSettings* build_settings,
     AddSysrootDependencyToCrate(&crate, sysroot, "core");
     AddSysrootDependencyToCrate(&crate, sysroot, "alloc");
     AddSysrootDependencyToCrate(&crate, sysroot, "std");
+
+    // Proc macros have the proc_macro crate as a direct dependency
+    if (std::string_view(rust_tool->name()) ==
+        std::string_view(RustTool::kRsToolMacro)) {
+      AddSysrootDependencyToCrate(&crate, sysroot, "proc_macro");
+    }
   }
 
   // Add the rest of the crate dependencies.
