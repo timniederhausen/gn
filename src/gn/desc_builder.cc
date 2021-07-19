61a0153b5e60f11100a04e51393cae15268d1ee3
Eli Lipsitz <elipsitz@fb.com>
2021-07-19 11:27:41 -0700

    Add 'rustenv' to 'gn desc' output
    This commit adds each target's 'rustenv' property, which stores
    key/value environment variables to pass to the Rust compiler,
    in the output of 'gn desc'.
    
    This also fixes a few typos in code comments in the 'desc' command.
    
    Change-Id: I3e0995dab2656ad23fbce9069eb7a83aa4e4cb9d
    Reviewed-on: https://gn-review.googlesource.com/c/gn/+/12020
    Reviewed-by: Brett Wilson <brettw@chromium.org>
    Commit-Queue: Brett Wilson <brettw@chromium.org>

--- src/gn/desc_builder.cc
+++ src/gn/desc_builder.cc
@@ -44,7 +44,9 @@
 //   "depfile : "file name for action input dependencies",
 //   "outputs" : [ list of target outputs ],
 //   "arflags", "asmflags", "cflags", "cflags_c",
-//   "clfags_cc", "cflags_objc", "clfags_objcc" : [ list of flags],
+//   "cflags_cc", "cflags_objc", "cflags_objcc",
+//   "rustflags" : [ list of flags],
+//   "rustenv" : [ list of Rust environment variables ],
 //   "defines" : [ list of preprocessor definitions ],
 //   "include_dirs" : [ list of include directories ],
 //   "precompiled_header" : "name of precompiled header file",
@@ -513,6 +515,8 @@ class TargetDescBuilder : public BaseDescBuilder {
       CONFIG_VALUE_ARRAY_HANDLER(cflags_objcc, std::string,
                                  kRecursiveWriterKeepDuplicates)
       CONFIG_VALUE_ARRAY_HANDLER(rustflags, std::string,
+                                 kRecursiveWriterKeepDuplicates)
+      CONFIG_VALUE_ARRAY_HANDLER(rustenv, std::string,
                                  kRecursiveWriterKeepDuplicates)
       CONFIG_VALUE_ARRAY_HANDLER(defines, std::string,
                                  kRecursiveWriterSkipDuplicates)
