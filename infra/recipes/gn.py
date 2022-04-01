0b087f15381f50ed6fd580cea0c0efd58096d3ea
Takuto Ikuta <tikuta@google.com>
2022-04-01 09:52:59 +0900

    Reland "make recipes python3 compatible"
    This reverts commit 93f0d7a7d3ade923b6d0b36b3df0ffacbe7e9f5d.
    
    Reason for revert:
    use /dev/null instead of '-' (stdin) for xcrun toolchain step.
    
    Confirmed with
    $ led get-build 8818214129795863857 | led edit-recipe-bundle | \
      led launch
    https://ci.chromium.org/raw/build/logs.chromium.org/gn/led/tikuta_google.com/7595c787826548d51e7f033368220d2595ca3e39179dffb6c2c72d93762af62a/+/build.proto?server=chromium-swarm.appspot.com
    
    Original change's description:
    > Revert "make recipes python3 compatible"
    >
    > This reverts commit ee7f7afa7e606888e36c2a7dd9d44e8c8040051e.
    >
    > Reason for revert:
    > Culprit of timeout in
    > https://ci.chromium.org/ui/p/gn/builders/try/mac/b8818214129795863857/overview
    >
    > Original change's description:
    > > make recipes python3 compatible
    > >
    > > This updates recipes.py with
    > > $ curl https://chromium.googlesource.com/infra/luci/recipes-py/+/1b1ecd03e0b00399784c43add1465f685b6d1ab9/recipes.py?format=TEXT |\
    > >    base64 -d > infra/recipes.py
    > >
    > > And fixed some str/bytes usage in step/raw_io recipe modules.
    > >
    > > Bug: 283
    > > Change-Id: Ib714244beb8971dcbd462544069fcb60c9b58a01
    > > Reviewed-on: https://gn-review.googlesource.com/c/gn/+/13400
    > > Reviewed-by: Petr Hosek <phosek@google.com>
    > > Commit-Queue: Takuto Ikuta <tikuta@google.com>
    >
    > TBR=phosek@google.com,brettw@google.com,tikuta@google.com
    >
    > Change-Id: If4fe0fe2a1ca487e98d78992eebb8bcb058eb060
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Bug: 283
    > Reviewed-on: https://gn-review.googlesource.com/c/gn/+/13441
    > Reviewed-by: Brett Wilson <brettw@chromium.org>
    > Commit-Queue: Brett Wilson <brettw@chromium.org>
    
    # Not skipping CQ checks because this is a reland.
    
    Bug: 283
    Change-Id: I39dcbd060abe2d7137352a71dcb0e13a3340d9b4
    Reviewed-on: https://gn-review.googlesource.com/c/gn/+/13480
    Reviewed-by: Brett Wilson <brettw@chromium.org>
    Commit-Queue: Takuto Ikuta <tikuta@google.com>

--- infra/recipes/gn.py
+++ infra/recipes/gn.py
@@ -5,6 +5,8 @@
 
 from recipe_engine.recipe_api import Property
 
+PYTHON_VERSION_COMPATIBILITY = 'PY2+3'
+
 DEPS = [
     'recipe_engine/buildbucket',
     'recipe_engine/cipd',
@@ -42,10 +44,10 @@ def _get_libcxx_include_path(api):
   lines = api.step(
       'xcrun toolchain', [
           'xcrun', '--toolchain', 'clang', 'clang++', '-xc++', '-fsyntax-only',
-          '-Wp,-v', '-'
+          '-Wp,-v', '/dev/null'
       ],
-      stderr=api.raw_io.output(name='toolchain', add_output_log=True),
-      step_test_data=lambda: api.raw_io.test_api.stream_output(
+      stderr=api.raw_io.output_text(name='toolchain', add_output_log=True),
+      step_test_data=lambda: api.raw_io.test_api.stream_output_text(
           str(api.macos_sdk.sdk_dir.join('include', 'c++', 'v1')),
           stream='stderr')).stderr.splitlines()
   # Iterate over all include paths and look for the SDK libc++ one.
@@ -72,8 +74,8 @@ def _get_compilation_environment(api, target, cipd_dir):
     triple = '--target=%s' % target.triple
     sysroot = '--sysroot=%s' % api.step(
         'xcrun sdk-path', ['xcrun', '--show-sdk-path'],
-        stdout=api.raw_io.output(name='sdk-path', add_output_log=True),
-        step_test_data=lambda: api.raw_io.test_api.stream_output(
+        stdout=api.raw_io.output_text(name='sdk-path', add_output_log=True),
+        step_test_data=lambda: api.raw_io.test_api.stream_output_text(
             '/some/xcode/path')).stdout.strip()
     stdlib = cipd_dir.join('lib', 'libc++.a')
     cxx_include = _get_libcxx_include_path(api)
@@ -117,7 +119,7 @@ def RunSteps(api, repository):
       api.step('checkout', ['git', 'checkout', 'FETCH_HEAD'])
       revision = api.step(
           'rev-parse', ['git', 'rev-parse', 'HEAD'],
-          stdout=api.raw_io.output()).stdout.strip()
+          stdout=api.raw_io.output_text()).stdout.strip()
       for change in build_input.gerrit_changes:
         api.step('fetch %s/%s' % (change.change, change.patchset), [
             'git', 'fetch', repository,
@@ -319,7 +321,7 @@ def GenTests(api):
       git_repo='gn.googlesource.com/gn',
       revision='a' * 40,
   ) + api.step_data(
-      'git.rev-parse', api.raw_io.stream_output('a' * 40)
+      'git.rev-parse', api.raw_io.stream_output_text('a' * 40)
   ) + api.step_data(
       'release.linux-amd64.upload.cipd search gn/gn/linux-amd64 git_revision:' +
       'a' * 40,
@@ -331,7 +333,7 @@ def GenTests(api):
       git_repo='gn.googlesource.com/gn',
       revision='a' * 40,
   ) + api.step_data(
-      'git.rev-parse', api.raw_io.stream_output('a' * 40)
+      'git.rev-parse', api.raw_io.stream_output_text('a' * 40)
   ) + api.step_data(
       'release.linux-amd64.upload.cipd search gn/gn/linux-amd64 git_revision:' +
       'a' * 40, api.cipd.example_search('gn/gn/linux-amd64', [])))
