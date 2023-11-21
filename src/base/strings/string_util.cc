92e63272dc04ee7cc1eaae2633c25da06e4f9c03
Takuto Ikuta <tikuta@google.com>
2023-11-21 17:44:35 +0900

    remove unused function
    Change-Id: I33472f3318dc10292366bd2db9438f560eb8153b
    Reviewed-on: https://gn-review.googlesource.com/c/gn/+/16260
    Reviewed-by: Petr Hosek <phosek@google.com>
    Commit-Queue: Takuto Ikuta <tikuta@google.com>

--- src/base/strings/string_util.cc
+++ src/base/strings/string_util.cc
@@ -1018,29 +1018,4 @@ std::u16string ReplaceStringPlaceholders(const std::u16string& format_string,
   return result;
 }
 
-// The following code is compatible with the OpenBSD lcpy interface.  See:
-//   http://www.gratisoft.us/todd/papers/strlcpy.html
-//   ftp://ftp.openbsd.org/pub/OpenBSD/src/lib/libc/string/{wcs,str}lcpy.c
-
-namespace {
-
-template <typename CHAR>
-size_t lcpyT(CHAR* dst, const CHAR* src, size_t dst_size) {
-  for (size_t i = 0; i < dst_size; ++i) {
-    if ((dst[i] = src[i]) == 0)  // We hit and copied the terminating NULL.
-      return i;
-  }
-
-  // We were left off at dst_size.  We over copied 1 byte.  Null terminate.
-  if (dst_size != 0)
-    dst[dst_size - 1] = 0;
-
-  // Count the rest of the |src|, and return it's length in characters.
-  while (src[dst_size])
-    ++dst_size;
-  return dst_size;
-}
-
-}  // namespace
-
 }  // namespace base
