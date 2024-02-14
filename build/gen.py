a3dcd7a7ad861a9c1b9930a654f2feaeb03b4c0c
Reito <carolwolfking@gmail.com>
2024-02-14 16:47:31 +0800

    Add long path support for windows
    Change-Id: I84a25207a08e8590288855c757b0241c4a8a9fed
    Reviewed-on: https://gn-review.googlesource.com/c/gn/+/16600
    Reviewed-by: David Turner <digit@google.com>
    Reviewed-by: Takuto Ikuta <tikuta@google.com>
    Commit-Queue: Takuto Ikuta <tikuta@google.com>

--- build/gen.py
+++ build/gen.py
@@ -148,7 +148,11 @@ class ArgumentsList:
           result.append('%s=%s' % (long_option, item))
       else:
         assert action is None, "Unsupported action " + action
-    return ' '.join(shell_quote(item) for item in result)
+
+    if platform.system() == "Windows":
+      return ' '.join(result)
+    else:
+      return ' '.join(shell_quote(item) for item in result)
 
 
 def main(argv):
@@ -175,7 +179,8 @@ def main(argv):
                     help='Enable the use of UndefinedBehaviorSanitizer')
   args_list.add('--no-last-commit-position', action='store_true',
                     help='Do not generate last_commit_position.h.')
-  args_list.add('--out-path',
+  args_list.add('--out-path', type=str,
+                    default=os.path.join(REPO_ROOT, 'out'),
                     help='The path to generate the build files in.')
   args_list.add('--no-strip', action='store_true',
                     help='Don\'t strip release build. Useful for profiling.')
@@ -214,7 +219,7 @@ def main(argv):
   else:
     host = platform
 
-  out_dir = options.out_path or os.path.join(REPO_ROOT, 'out')
+  out_dir = options.out_path
   if not os.path.isdir(out_dir):
     os.makedirs(out_dir)
   if not options.no_last_commit_position:
@@ -566,7 +571,10 @@ def WriteGNNinja(path, platform, host, options, args_list):
         '/D_HAS_EXCEPTIONS=0',
     ])
 
-    ldflags.extend(['/DEBUG', '/MACHINE:x64'])
+    win_manifest = os.path.relpath(
+      os.path.join(REPO_ROOT, "build/windows.manifest.xml"), options.out_path)
+    ldflags.extend(['/DEBUG', '/MACHINE:x64', '/MANIFEST:EMBED',
+                    f'/MANIFESTINPUT:{win_manifest}'])
 
   static_libraries = {
       'base': {'sources': [
